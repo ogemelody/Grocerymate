46127bb56437127d32f61d7a6af2d3c95ef062ce
Melody Egwuchukwu <melodyegwuchukwu1@gmail.com>
2025-06-10 12:30:51 +0200

    add sg for alb & asg

--- infrastructure/modules/networking/main.tf
+++ infrastructure/modules/networking/main.tf
@@ -158,3 +158,53 @@ resource "aws_security_group" "db_sg" {
     Name = "${var.vpc_name}-db-sg"
   }
 }
+
+#ALB
+resource "aws_security_group" "alb_sg" {
+  name   = "alb-sg"
+  vpc_id = aws_vpc.grocery-app-vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#Security group ASG
+resource "aws_security_group" "asg_sg" {
+  name        = "asg-sg"
+  description = "Allow SSH and app traffic"
+  vpc_id      = aws_vpc.grocery-app-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
