02767e1e5950a7d8518121af21b8a90314aa1a59
Melody Egwuchukwu <melodyegwuchukwu1@gmail.com>
2025-06-10 03:27:27 +0200

    add key pair

--- infrastructure/modules/EC2/main.tf
+++ infrastructure/modules/EC2/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "app_server" {
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [var.security_group_id]
   associate_public_ip_address = true
+  key_name                   = var.key_name
 
   tags = {
     Name = var.instance_name
--- infrastructure/modules/EC2/variables.tf
+++ infrastructure/modules/EC2/variables.tf
@@ -23,3 +23,10 @@ variable "instance_name" {
   description = "Name tag for the EC2 instance"
   type        = string
 }
+
+
+variable "key_name" {
+  description = "The name of the key pair to use for the EC2 instance"
+  type        = string
+  default     = ""
+}
