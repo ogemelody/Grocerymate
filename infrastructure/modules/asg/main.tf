7408f8237dc16bf7f2530dd9fe49488a350fa08e
Melody Egwuchukwu <melodyegwuchukwu1@gmail.com>
2025-06-16 14:14:17 +0200

    update asg with network interface for EC2

--- infrastructure/modules/asg/main.tf
+++ infrastructure/modules/asg/main.tf
@@ -5,8 +5,10 @@ resource "aws_autoscaling_group" "grocerymate_asg" {
   min_size             = var.min_size
   vpc_zone_identifier  = var.public_subnet_ids
 
+
   launch_template {
     id      = var.launch_template_id
+
     version = "$Latest"
   }
 
@@ -21,6 +23,7 @@ resource "aws_autoscaling_group" "grocerymate_asg" {
 resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.grocerymate_asg.id
   lb_target_group_arn    = var.target_group_arn
+
 }
 
 
